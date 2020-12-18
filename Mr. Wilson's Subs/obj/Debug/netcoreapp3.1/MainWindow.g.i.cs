bb35ea0e48b475553e73da44f797b035f731ad77
kkiser21 <54853058+kkiser21@users.noreply.github.com>
2020-12-18 12:38:42 -0800

    ngerifgjunsodi

--- Mr. Wilson's Subs/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Mr. Wilson's Subs/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -213,7 +213,7 @@ namespace Mr._Wilson_s_Subs {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Mr. Wilson\'s Subs;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Mr. Wilson\'s Subs;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
